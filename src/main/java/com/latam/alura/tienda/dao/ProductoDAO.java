24055ed9fe3f94bb75e7596f1b7c51f1b07c67eb
MolinAnimation <Molinanimation@gmail.com>
2023-09-20 14:41:25 -0500

    filtros de consulta en productoDAO

--- src/main/java/com/latam/alura/tienda/dao/ProductoDAO.java
+++ src/main/java/com/latam/alura/tienda/dao/ProductoDAO.java
@@ -1,5 +1,8 @@
 package com.latam.alura.tienda.dao;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 import com.latam.alura.tienda.modelo.Producto;
@@ -16,4 +19,27 @@ public class ProductoDAO {
         this.em.persist(producto);
     }
 
+    public Producto consultaPorId(Long id) {
+        return em.find(Producto.class, id);
+    }
+
+    public List<Producto> consultarTodos() {
+        String jpql = "SELECT P FROM Producto AS P";
+        return em.createQuery(jpql, Producto.class).getResultList();
+    }
+
+    public List<Producto> consultarPorNombre(String nombre) {
+        String jpql = " SELECT P FROM Producto AS P WHERE P.nombre=:nombre";
+        return em.createQuery(jpql, Producto.class).setParameter("nombre", nombre).getResultList();
+    }
+
+    public List<Producto> consultaPorNombreDeCategoria(String nombre) {
+        String jpql = "SELECT P FROM Producto AS P WHERE P.categoria.nombre=:nombre";
+        return em.createQuery(jpql, Producto.class).setParameter("nombre", nombre).getResultList();
+    }
+
+    public BigDecimal consultarPrecioPorNombreDeProducto(String nombre) {
+        String jpql = "SELECT P.precio FROM Producto AS P WHERE P.nombre=:nombre";
+        return em.createQuery(jpql, BigDecimal.class).setParameter("nombre", nombre).getSingleResult();
+    }
 }
--- src/main/java/com/latam/alura/tienda/prueba/RegistroDeProducto.java
+++ src/main/java/com/latam/alura/tienda/prueba/RegistroDeProducto.java
@@ -1,6 +1,7 @@
 package com.latam.alura.tienda.prueba;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -15,25 +16,32 @@ import com.latam.alura.tienda.utils.JPAUtils;
 public class RegistroDeProducto {
 
 	public static void main(String[] args) {
+		registrarProducto();
+		EntityManager em = JPAUtils.getEntityManager();
+		ProductoDAO productoDAO = new ProductoDAO(em);
+		Producto producto = productoDAO.consultaPorId(1l);
+		System.out.println(producto.getNombre());
+
+		BigDecimal precio = productoDAO.consultarPrecioPorNombreDeProducto("xiaomi redmi");
+		System.out.println(precio);
+	}
+
+	private static void registrarProducto() {
 		Categoria celulares = new Categoria("CELULARES");
 
+		Producto celular = new Producto("xiaomi redmi", "muito legal", new BigDecimal("800"), celulares);
+
 		EntityManager em = JPAUtils.getEntityManager();
+		ProductoDAO productoDAO = new ProductoDAO(em);
+		CategoriaDAO categoriaDAO = new CategoriaDAO(em);
 
 		em.getTransaction().begin();
 
-		em.persist(celulares);
+		categoriaDAO.guardar(celulares);
+		productoDAO.guardar(celular);
 
-		celulares.setNombre("Libros");
-
-		em.flush();
-		em.clear();
-
-		celulares = em.merge(celulares);
-
-		celulares.setNombre("softwares");
-
-		em.flush();
-
+		em.getTransaction().commit();
+		em.close();
 	}
 
 }
